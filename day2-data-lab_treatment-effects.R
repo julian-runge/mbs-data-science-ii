5d6da3f04445be1ee571bcec754a20fba6061d3c
Julian Runge <julian.runge@gmail.com>
2023-12-07 17:23:18 +0100

    Update day2-data-lab_treatment-effects.R

--- day2-data-lab_treatment-effects.R
+++ day2-data-lab_treatment-effects.R
@@ -12,7 +12,7 @@ pkgs <- list("glmnet", "reticulate", "stringr", "rstudioapi", "data.table", "par
 lapply(pkgs, require, character.only = T)
 
 # set wd
-setwd("/Users/jr/Dropbox/Talks:teaching/Mannheim Business School/Data")
+setwd("TBD")
 
 # load cleaned and prepared datasets
 data <- read.csv("day2-data.csv", header=TRUE, sep=",")
@@ -76,10 +76,30 @@ summarySE(data, "d30_rounds_played", groupvars = c("test_bucket"), na.rm = FALSE
 summarySE(data, "d30_retention", groupvars = c("test_bucket"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
 summarySE(data, "d30_ad_views", groupvars = c("test_bucket"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
 
+summarySE(data, "d60_revenue", groupvars = c("test_bucket"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+
+data$d60_rev_denoised <- data$d60_revenue
+data$d60_rev_denoised[data$d60_revenue > quantile(filter(data,d60_revenue > 0)$d60_revenue, c(d))] <- quantile(filter(data,d60_revenue > 0)$d60_revenue, c(d))
+summarySE(data, "d60_rev_denoised", groupvars = c("test_bucket"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+
+
+### country and device tier
+summarySE(data, "d30_offer_conversion", groupvars = c("country_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_offer_revenue", groupvars = c("country_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_conversion", groupvars = c("country_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_revenue", groupvars = c("country_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_rev_denoised", groupvars = c("country_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+
+summarySE(data, "d30_offer_conversion", groupvars = c("device_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_offer_revenue", groupvars = c("device_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_conversion", groupvars = c("device_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_revenue", groupvars = c("device_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+summarySE(data, "d30_rev_denoised", groupvars = c("device_tier"), na.rm = FALSE, conf.interval = 0.95, .drop = TRUE)
+
 ### visualize treatment effects
 
 # using test_bucket
-plotmeans(d7_revenue ~ interaction(test_bucket, sep ="   "),
+plotmeans(d30_revenue ~ interaction(test_bucket, sep ="   "),
           connect=list(1:4),
           barwidth=2,
           col="dark green",
@@ -88,8 +108,40 @@ plotmeans(d7_revenue ~ interaction(test_bucket, sep ="   "),
           #data=analysis_data3,
           data=data,
           xlab="",
-          ylab="Day 7 Gross Revenue",
-          ylim=c(0.5,1.2),
+          ylab="Day 30 Gross Revenue",
+          ylim=c(0.5,3.5),
+          text.n.label="",
+          #n.label=FALSE,
+          main=c(""),
+          p=0.95)
+
+plotmeans(d30_rev_denoised ~ interaction(test_bucket, sep ="   "),
+          connect=list(1:4),
+          barwidth=2,
+          col="dark green",
+          barcol="dark green",
+          ccol="dark green",
+          #data=analysis_data3,
+          data=data,
+          xlab="",
+          ylab="Day 30 Revenue (Denoised)",
+          ylim=c(0.5,2.5),
+          text.n.label="",
+          #n.label=FALSE,
+          main=c(""),
+          p=0.95)
+
+plotmeans(d60_rev_denoised ~ interaction(test_bucket, sep ="   "),
+          connect=list(1:4),
+          barwidth=2,
+          col="dark green",
+          barcol="dark green",
+          ccol="dark green",
+          #data=analysis_data3,
+          data=data,
+          xlab="",
+          ylab="Day 60 Revenue (Denoised)",
+          ylim=c(2.3,3.8),
           text.n.label="",
           #n.label=FALSE,
           main=c(""),
@@ -100,7 +152,7 @@ data$action2 <- as.factor(data$action2)
 data$treatment_group <- ordered(data$action2,
                         levels = c("2.99 USD", "4.99 USD", "29.99 USD", "Control"))
 
-plotmeans(d7_revenue ~ interaction(treatment_group, sep ="   "),
+plotmeans(d30_rev_denoised ~ interaction(treatment_group, sep ="   "),
           connect=list(1:4),
           barwidth=2,
           col="dark green",
@@ -109,8 +161,8 @@ plotmeans(d7_revenue ~ interaction(treatment_group, sep ="   "),
           #data=analysis_data3,
           data=data,
           xlab="",
-          ylab="Day 7 Gross Revenue",
-          ylim=c(0.5,1.2),
+          ylab="Day 30 Revenue (Denoised)",
+          ylim=c(1.2,2.2),
           text.n.label="",
           #n.label=FALSE,
           main=c(""),
@@ -124,3 +176,27 @@ data$action2 <- relevel(data$action2, ref = "Control")
 lm_treat = lm(d30_rev_denoised ~ action2, data = data)
 
 summary(lm_treat) 
+
+lm_treat2 = lm(d30_conversion ~ action2, data = data)
+
+summary(lm_treat2) 
+
+# look at associations between device memory and user spending
+lm_ram = lm(d30_rev_denoised ~ device_ram, data = data)
+
+summary(lm_ram)
+
+data$device_ram_gb <- data$device_ram/1000
+lm_ram_gb = lm(d30_rev_denoised ~ device_ram_gb, data = data)
+
+summary(lm_ram_gb)
+
+lm_ram_gb2 = lm(d30_revenue ~ device_ram_gb, data = data)
+
+summary(lm_ram_gb2)
+
+# look at interaction effects between treatments and device memory
+
+lm_ram_treat2 = lm(d30_rev_denoised ~ device_ram_gb*action2, data = data)
+
+summary(lm_ram_treat2)
